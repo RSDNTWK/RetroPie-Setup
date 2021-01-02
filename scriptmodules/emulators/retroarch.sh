e0c470e87f3d50fe4913e8aa840403af95b8c933
RSDNTWK <admin@raidensnakesden.net>
2021-01-02 08:57:15 +0000

    deleted shaders

--- scriptmodules/emulators/retroarch.sh
+++ scriptmodules/emulators/retroarch.sh
@@ -88,7 +88,6 @@ function update_shaders_retroarch() {
     [[ ! -d "$dir/.git" ]] && rm -rf "$dir"
     gitPullOrClone "$dir" https://github.com/RetroPie/common-shaders.git "$branch"
 	[[ ! -d "$dir/.git" ]] && rm -rf "$dir/.git"
-	gitPullOrClone "$dir" https://github.com/libretro/slang-shaders.git "$branch"
     chown -R $user:$user "$dir"
 }
 
