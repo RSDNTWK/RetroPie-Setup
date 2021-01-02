03349d44c8d532796f10ee5d31a99ccfdec12aa4
RSDNTWK <admin@raidensnakesden.net>
2021-01-02 12:58:44 +0000

    add ppsspp 1.5.4

--- scriptmodules/emulators/ppsspp-1.5.4.sh
+++ scriptmodules/emulators/ppsspp-1.5.4.sh
@@ -14,7 +14,7 @@ rp_module_desc="PlayStation Portable emulator PPSSPP v1.5.4"
 rp_module_help="ROM Extensions: .iso .pbp .cso\n\nCopy your PlayStation Portable roms to $romdir/psp"
 rp_module_licence="GPL2 https://raw.githubusercontent.com/hrydgard/ppsspp/master/LICENSE.TXT"
 rp_module_section="opt"
-rp_module_flags="!all videocore"
+rp_module_flags="videocore"
 
 function depends_ppsspp-1.5.4() {
     depends_ppsspp
